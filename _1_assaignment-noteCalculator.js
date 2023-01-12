daf6e0cb7821b2e999be98dbe01f0f331c566872
janvaljean <kurtuldu@gmx.de>
2023-01-12 14:49:18 +0100

    noteCalculator

---
+++ _1_assaignment-noteCalculator.js
@@ -0,0 +1,26 @@
+// if you enter false ,program ask you to enter new note
+while (true) {
+let yourNote = +prompt('Please enter your note:');
+if (yourNote <= 25) {
+    console.log("yourNote is FF");
+    break;
+} else if (25< yourNote & yourNote <= 45) {
+    console.log("yourNote is DD");
+    break;
+} else if (45< yourNote & yourNote  <= 65) {
+    console.log("yourNote is CC");
+    break;
+} else if (65< yourNote & yourNote  <= 75) {
+    console.log("yourNote is BB");
+    break;
+} else if (75< yourNote & yourNote  <= 90) {
+    console.log("yourNote is BA");
+    break;
+} else if (90< yourNote & yourNote  <= 100) {
+    console.log("yourNote is AA");
+    break;
+} else{
+    alert("Your note is out of range or false");
+    alert("enter a valid note");
+}}
+
